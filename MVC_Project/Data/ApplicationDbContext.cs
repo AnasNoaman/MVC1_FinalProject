207a2a1d7912010f9aa2f80447fde3e0be681387
Anas Noaman <147315220+AnasNoaman@users.noreply.github.com>
2025-10-09 14:20:12 +0300

    first commit

---
+++ MVC_Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Project.Models;
+
+namespace MVC_Project.Data
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=MVC_Project;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Mobiles" },
+                new Category { Id = 2, Name = "Laptops" },
+                new Category { Id = 3, Name = "Tablets" }
+            );
+
+        }
+    }
+}
