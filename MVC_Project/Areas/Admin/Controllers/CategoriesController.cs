46f0e11e6d24e278c7228e8319c1229cb94d427b
Anas Noaman <147315220+AnasNoaman@users.noreply.github.com>
2025-10-16 16:33:48 +0300

    create

---
+++ MVC_Project/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Project.Data;
+using MVC_Project.Models;
+
+namespace MVC_Project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+
+        public IActionResult Index()
+        {
+            var Categores = context.Categories.ToList();
+            return View(Categores);
+        }
+        public IActionResult Remove(int id)
+        {
+            var cat = context.Categories.Find(id);
+            context.Categories.Remove(cat);
+            context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Edit(int id) { 
+            var cat = context.Categories.Find(id);
+            return View(nameof(Edit),cat);
+        }
+        public IActionResult Update(Category category)
+        {
+
+            if (!ModelState.IsValid) { 
+            return View(nameof(Edit),category);
+            }
+            var cat = context.Categories.Find(category.Id);
+            cat.Name = category.Name;
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index)); 
+        }
+        public IActionResult Create() { 
+            return View();
+        }
+        public IActionResult Store(Category category) { 
+        
+            if (!ModelState.IsValid)
+            {
+                return View(nameof(Create), category);
+            }
+            
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
--- MVC_Project/Models/Category.cs
+++ MVC_Project/Models/Category.cs
@@ -1,8 +1,14 @@
-﻿namespace MVC_Project.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Project.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
+
         public string Name { get; set; }
         public List<Product> Products { get; set; }=new List<Product>();
     }
